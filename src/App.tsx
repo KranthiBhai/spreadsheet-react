8dfb5b5421fe69453b9dcd84eaff5a33ee657e07
KranthiBhai <95521380+KranthiBhai@users.noreply.github.com>
2022-03-31 11:55:49 +0530

    created two diiferent kinds of examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Spreadsheet from 'react-spreadsheet';
+import SpreadSheetData from "./SpreadSheet.json";
+import DynamicTable from './DynamicTable';
+
 
 function App() {
+
+  const titles = SpreadSheetData.map((obj) => {
+    return obj.row.map((title)=>{ return title.Heading})
+  })
+  
+  console.log(titles)
+
+  const tableData= [
+    [{ value: "Heading1" }, { value: "Heading2" },],
+    [{ value: "Vanilla" }, { value: "Chocolate" }],
+    [{ value: "Strawberry" }, { value: "Cookies" }],
+  ]
+
+  const [data, setData] = useState(tableData);
+
+
+  const onChange = (data:any) => {
+    return setData(data)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+  {/* Type1 */}
+  <div>
+    <button style={{background: "blue", color: "white", fontSize: "20px", fontWeight: "bold", borderRadius: "10px", cursor: "pointer", margin: "10px", paddingLeft: "10px", paddingRight: "10px", paddingTop: "5px", paddingBottom: "5px"}}>Add Cell</button>
+  </div>
+    <Spreadsheet data={data} onChange={onChange} />
+
+    {/* Type2 */}
+    <DynamicTable />
+  </>
+  )
 }
 
 export default App;
